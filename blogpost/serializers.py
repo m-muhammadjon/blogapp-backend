639a14a2a7b8972c4db004b84db9dc0de4f32565
Mukhammadjon <muhammadjon.200415@gmail.com>
2022-09-03 03:27:55 +0500

    post like action updated

--- blogpost/serializers.py
+++ blogpost/serializers.py
@@ -17,16 +17,20 @@ class PostSerializer(serializers.ModelSerializer):
     comments = serializers.SerializerMethodField()
     author = serializers.ReadOnlyField(source='author.username')
     likes = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ('id', 'author', 'title', 'body', 'image', 'created_at', 'updated_at', 'comments', 'likes')
+        fields = ('id', 'author', 'title', 'body', 'image', 'created_at', 'updated_at', 'comments', 'likes', 'is_liked')
 
     def get_comments(self, obj):
         return CommentShortSerializer(obj.comments, many=True).data
 
     def get_likes(self, obj):
         return obj.get_total_likes()
+
+    def get_is_liked(self, obj):
+        return self.context['request'].user in obj.likes.all()
 
 
 class CommentSerializer(serializers.ModelSerializer):
